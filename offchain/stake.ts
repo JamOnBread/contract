cd3a1d671680fc4328a9d1d8d87f096a164eafbb
Miksa <martin.miksanik@vacuumlabs.com>
2023-10-16 10:29:10 +0200

    Add types for instant buy

--- offchain/stake.ts
+++ offchain/stake.ts
@@ -3,7 +3,7 @@ import { mintUniqueAsset, encodeTreasuryDatumTokens, getCompiledCodeParams } fro
 
 
 const privKey = "ed25519_sk1z5zd4ap8nyyvlh2uz5rt08xh76yjhs0v7yv58vh00z399m3vrppqfhxv0n" // Treaury
-const jobPolicy = "993f5dfae7554d2dd6c570b224afad6ab9ca2a6bdd0aecbd4864b0ae" as PolicyId
+const jobPolicy = "74ce41370dd9103615c8399c51f47ecee980467ecbfcfbec5b59d09a" as PolicyId
 const jobAssetName = "556e69717565"
 const lucid = await Lucid.new(
     new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0", "preprodVm9mYgzOYXlfFrFYfgJ2Glz7AlnMjvV9"),
@@ -13,7 +13,7 @@ lucid.selectWalletFromPrivateKey(privKey)
 const pool = "pool13m26ky08vz205232k20u8ft5nrg8u68klhn0xfsk9m4gsqsc44v"
 const stakeValidator = getCompiledCodeParams(
     'staking.withdrawal_v1', 
-    [encodeTreasuryDatumTokens("74ce41370dd9103615c8399c51f47ecee980467ecbfcfbec5b59d09a", 1n), 1n]
+    [encodeTreasuryDatumTokens("74ce41370dd9103615c8399c51f47ecee980467ecbfcfbec5b59d09a", 1n), 2n]
 )
 
 const stake = lucid.utils.validatorToScriptHash(stakeValidator)
@@ -25,7 +25,7 @@ console.log("Reward", reward)
 
 
 const tx = await lucid.newTx()
-    //.registerStake(reward)
+    .registerStake(reward)
     .delegateTo(reward, pool, Data.void())
     .payToAddress(await lucid.wallet.address(), { [jobPolicy + jobAssetName]: 6n })
     .attachCertificateValidator(stakeValidator)
@@ -33,7 +33,6 @@ const tx = await lucid.newTx()
 
 const signedTx = await tx.sign().complete()
 const txHash = await signedTx.submit()
-
 console.log(txHash)
 
 /*
